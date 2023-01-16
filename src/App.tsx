6823127306e8c713e8bda700e0e06ae843a511f6
Kallnur <kalnur.go@gmail.com>
2023-01-17 00:38:23 +0600

    contr

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Keplr, Key } from '@keplr-wallet/types';
 import { SetStateAction, useEffect, useState } from 'react'
 import './App.css'
+import Chaines from './components/chianes/chaines';
 import Form from './components/form/form';
 import Modal from './components/modal/modal';
 import UserCard from './components/userCard/userCard';
@@ -10,25 +11,9 @@ const COSMOS_ID = "cosmoshub-4";
 
 function App() {
 
-  const [userInfo, setUserInfo] = useState<Key | undefined>();
-  const [showModal, setShowModal] = useState<boolean>(true)
-
-  useEffect(() => {
-    getData(COSMOS_ID).then(async (keplr: Keplr | undefined) => {
-      const userInfo = await keplr?.getKey(COSMOS_ID);
-      return userInfo
-    }).then((userInfo) => {
-      setUserInfo(userInfo)
-    })
-    
-  }, [])
-
   return (
     <div className="App">
-      <UserCard userInfo={userInfo} setShowModal={setShowModal}/>
-      <Modal showModal={showModal} setShowModal={setShowModal}>
-        <Form COSMOS_ID={COSMOS_ID}/>
-      </Modal>
+      <Chaines COSMOS_ID={COSMOS_ID}/>
     </div>
   )
 }
--- src/components/chianes/chaines.tsx
+++ src/components/chianes/chaines.tsx
@@ -1,11 +1,42 @@
-import {} from 'react'
+import { ChainInfoWithoutEndpoints } from '@keplr-wallet/types';
+import { useEffect, useState } from 'react'
+import { getData } from '../../keplr/start'
 
-interface Props {}
+interface Props {
+    COSMOS_ID: string
+}
 
-const Chaines = (props: Props) => {
+const Chaines = ({COSMOS_ID}: Props) => {
+
+    const [chaines, setChaines] = useState<ChainInfoWithoutEndpoints[] | null>(null);
+
+    const getChaines = () => {
+        const keplr = getData(COSMOS_ID);
+        keplr.then(async keplr => {
+            const chaines = await keplr?.getChainInfosWithoutEndpoints();
+            chaines && setChaines(chaines)
+        })
+    }
+
+    console.log(chaines);
+    
+
+    useEffect(() => {
+        getChaines();
+    }, [])
+
   return (
     <div>
-        
+        <ul>
+            {
+                chaines 
+                ? chaines.map(obj => (
+                    <li>
+                    </li>
+                ))
+                : null
+            }
+        </ul>
     </div>
   )
 }
